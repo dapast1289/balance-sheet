b41002e1763f5d623c6eaf58fd847c940237789a
leochen <leo@funpodium.net>
2021-01-19 14:00:05 +0800

    first commit

---
+++ src/main/java/com/dapast1289/googleSheets/balancesheet/BalanceSheetApplication.java
@@ -0,0 +1,13 @@
+package com.dapast1289.googleSheets.balancesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceSheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceSheetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dapast1289/googleSheets/balancesheet/BalanceSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dapast1289.googleSheets.balancesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceSheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
